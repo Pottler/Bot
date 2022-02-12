ab73f00fa02bfa5e08e48d02c35bde01430667ef
Pottler <pottlersecreto@gmail.com>
2022-02-12 11:34:03 -0600

    1

--- pottler.js
+++ pottler.js
@@ -3,21 +3,27 @@ const fs = require('fs');
 
 async function iniciar () {
         const client = new WAConnection()
-        client.logger.level = 'warn'
-        client.on('qr', () => {})
 
-        fs.existsSync('./pottler.json') &&
-client.loadAuthInfo('./pottler.json')
+        client.logger.level = 'warn'
+
+        client.on('qr', () => {
+        })
+
+        fs.existsSync('./Pottler.json') &&
+client.loadAuthInfo('./Pottler.json')
 
         client.on('connecting', () => {
-        console.log('Conectando')})
+        console.log('Conectando')
+        })
 
 
         client.on('open', () => {
-        console.log('Conectado exitosamente')})
+        console.log('Conectado exitosamente')
+        })
         await Client.connect({timeoutMS: 30*1000})
-        fs.writeFileSync('./pottler.json',
- JSON.stringify(client.base64EncodedAuthInfo(), null, '\t'))}
+        fs.writeFileSync('./Pottler.json',
+ JSON.stringify(client.base64EncodedAuthInfo(), null, '\t'))
+        }
 
 iniciar ()
 .catch (err => console.log("unexpected error: " + err))
